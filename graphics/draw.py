793e76cff659ae55e29e54d6e4a60957135ff257
backfrip <backfrip@users.noreply.github.com>
2016-02-25 03:12:33 +0000

    I totally can't read my work but I'm very proud of myself.

--- graphics/draw.py
+++ graphics/draw.py
@@ -15,62 +15,46 @@ def draw_line(screen, x0, y0, x1, y1, color):
     B = -(x1 - x0)
     m = float(A) / -B
     
-    a = x0
+    a = [x0]
     da = 1
     db = 1
     if m > 1: # II and VI
         d = [0, A + 2*B]
-        a = y0
-        b = x0
-        c = y1
+        a = [y0]
+        b = [x0]
+        c = [b, a]
+        f = y1
         d0 = -2*A
         d1 = -2*B
-        while a <= c:
-            screen.plot(b, a, color)
-            if d[0] > d[1]:
-                b += db
-                d[0] += d0
-            a += da
-            d[0] += d1
     elif m > 0: # I and V
         d = [2*A + B, 0]
-        b = y0
-        c = x1
+        b = [y0]
+        c = [a, b]
+        f = x1
         d0 = 2*B
         d1 = 2*A
-        while a <= c:
-            screen.plot(a, b, color)
-            if d[0] > d[1]:
-                b += db
-                d[0] += d0
-            a += da
-            d[0] += d1
     elif m > -1: # IV and VIII
         d = [0, 2*A - B]
-        b = y0
-        c = x1
+        b = [y0]
+        c = [a, b]
+        f = x1
         db = -1
         d0 = 2*B
         d1 = -2*A
-        while a <= c:
-            screen.plot(a, b, color)
-            if d[0] > d[1]:
-                b += db
-                d[0] += d0
-            a += da
-            d[0] += d1
     else: # III and VII
         d = [A - 2*B, 0]
-        a = y1
-        b = x1
-        c = y0
+        a = [y1]
+        b = [x1]
+        c = [b, a]
+        f = y0
         db = -1
         d0 = 2*A
         d1 = -2*B
-        while a <= c:
-            screen.plot(b, a, color)
-            if d[0] > d[1]:
-                b += db
-                d[0] += d0
-            a += da
-            d[0] += d1
+    
+    while a[0] <= f:
+        screen.plot(c[0][0], c[1][0], color)
+        if d[0] > d[1]:
+            b[0] += db
+            d[0] += d0
+        a[0] += da
+        d[0] += d1
